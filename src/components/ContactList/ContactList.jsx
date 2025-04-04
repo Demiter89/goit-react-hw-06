7cd65eadd16ab350651af4538177490d66a7ed2a
Maksym Shevchuk <maksymshevchuk89@gmai.com>
2025-04-04 13:54:29 +0200

    add index file

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -4,7 +4,7 @@ import css from "./ContactList.module.css";
 
 export default function ContactList() {
   const contacts = useSelector((state) => state.contacts.items);
-  const filter = useSelector((state) => state.filters.name);
+  const filter = useSelector((state) => state.filters.filter);
   const dispatch = useDispatch();
 
   const filteredContacts = contacts.filter((contact) =>
